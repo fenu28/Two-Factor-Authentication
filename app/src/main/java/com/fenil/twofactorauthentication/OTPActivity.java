7dc589427fbe51c3c516764be5d5dcb1a0079a3a
fenu28 <kamdarfenil88@gmail.com>
2021-06-28 12:38:34 +0530

    Added login, register and OTP activities

---
+++ app/src/main/java/com/fenil/twofactorauthentication/OTPActivity.java
@@ -0,0 +1,14 @@
+package com.fenil.twofactorauthentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_o_t_p);
+    }
+}
