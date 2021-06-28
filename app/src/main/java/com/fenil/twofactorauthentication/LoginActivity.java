77998047f48d20d5883458577670dd5878b71284
fenu28 <kamdarfenil88@gmail.com>
2021-06-28 12:56:30 +0530

    Linked UI components in LoginActivity.java

--- app/src/main/java/com/fenil/twofactorauthentication/LoginActivity.java
+++ app/src/main/java/com/fenil/twofactorauthentication/LoginActivity.java
@@ -3,12 +3,44 @@ package com.fenil.twofactorauthentication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText email, password;
+    Button signin, register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.edittext_email);
+        password = findViewById(R.id.edittext_password);
+        signin = findViewById(R.id.button_signin);
+        register = findViewById(R.id.button_register);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(email.getText().toString().isEmpty())
+                    Toast.makeText(LoginActivity.this,"Email ID field cannot be empty",Toast.LENGTH_SHORT).show();
+                else if(password.getText().toString().isEmpty())
+                    Toast.makeText(LoginActivity.this,"Password field cannot be empty",Toast.LENGTH_SHORT).show();
+                else
+                {
+
+                }
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
